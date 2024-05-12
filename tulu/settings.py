ef66f67127fc6c2db37f1b68c0cf3ad073b55c3a
adarshsavalagi1 <21ai005@sitmng.ac.in>
2024-05-13 01:17:57 +0530

    chore: Remove unused database configuration in settings.py

--- tulu/settings.py
+++ tulu/settings.py
@@ -63,10 +63,7 @@ WSGI_APPLICATION = 'tulu.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   
 }
 
 
